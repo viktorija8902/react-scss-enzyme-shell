defb53fec841feb4b262b323da1c61c9e2ba7b55
Viktorija <viktorija.bezsazna@gmail.com>
2018-09-30 11:34:02 -0400

    Add Enzyme

---
+++ src/tests/AnotherFile.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import AnotherFile from './../js/AnotherFile';
+
+describe('<AnotherFile />', () => {
+  it('renders three <Foo /> components', () => {
+    const wrapper = shallow(<AnotherFile test={"test in test"}/>);
+    // console.log(wrapper.debug())
+    expect(wrapper.text()).toEqual('test in test');
+  });
+
+  xit('renders an `.icon-star`', () => {
+    const wrapper = shallow(<MyComponent />);
+    expect(wrapper.find('.icon-star')).to.have.lengthOf(1);
+  });
+
+  xit('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
+  });
+});
