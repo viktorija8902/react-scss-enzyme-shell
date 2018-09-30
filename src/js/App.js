47ab583d88eb6d77d0f3113b1dc89e85e73d2e26
Viktorija <viktorija.bezsazna@gmail.com>
2018-09-30 13:02:24 -0400

    Add example API request

--- src/js/App.js
+++ src/js/App.js
@@ -4,6 +4,26 @@ import './../styles/App.scss';
 import AnotherFile from './AnotherFile.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: null, error: false };
+  }
+
+  componentDidMount() {
+    fetch(`https://api.openweathermap.org/data/2.5/box/city?bbox=15,50,120,100&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(response => {
+        console.log(response)
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`${response.status} ${response.statusText}`);
+        }
+      })
+      .then(data => this.setState({ data: JSON.stringify(data.list) }))
+      .catch(error => {
+        this.setState({ error: true})
+      });
+  }
   render() {
     return (
       <div className="App">
@@ -11,6 +31,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        {this.state.data}
+        {this.state.error && <div>Error!!</div>}
         <AnotherFile test={"test test test"}/>
       </div>
     );
